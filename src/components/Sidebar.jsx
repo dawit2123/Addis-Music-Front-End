cabd31bec2bf11ff4a4ff447c613a22179d075c0
dawit2123 <dawitzewdu2123@gmail.com>
2024-05-06 14:46:28 +0300

    color updated

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -30,7 +30,7 @@ const Sidebar = () => {
   const sidebarStyle = css`
     width: 250px;
     background-image: ${darkMode
-      ? "linear-gradient(to right, #000000, #4D4855)"
+      ? "linear-gradient(to right, rgba(0,0,0,0.89), #4D4855)"
       : "linear-gradient(160deg, #0093E9 0%, #80D0C7 100%)"};
     color: ${darkMode ? "white" : "black"};
     min-height: 90vh;
